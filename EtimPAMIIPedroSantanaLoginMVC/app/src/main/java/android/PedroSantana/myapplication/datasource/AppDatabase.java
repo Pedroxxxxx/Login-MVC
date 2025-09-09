7f4f1462d5574d50588a2046c24964f42974c86d
Pedroxxxxx <162354040+Pedroxxxxx@users.noreply.github.com>
2025-09-09 11:09:26 -0300

    Add files via upload

--- EtimPAMIIPedroSantanaLoginMVC/app/src/main/java/android/PedroSantana/myapplication/datasource/AppDatabase.java
+++ EtimPAMIIPedroSantanaLoginMVC/app/src/main/java/android/PedroSantana/myapplication/datasource/AppDatabase.java
@@ -1,4 +1,23 @@
 package android.PedroSantana.myapplication.datasource;
 
-public class AppDatabase    {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class AppDatabase extends SQLiteOpenHelper   {
+    public AppDatabase(@Nullable Context context) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
